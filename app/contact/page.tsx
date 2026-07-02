cecdf39a362154f5215b5a75726691b89c365dde
sanjay-kv <sanjay.kanakkot@gmail.com>
2026-07-02 12:07:11 +1000

    added the cbse changes and phone number

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -183,7 +183,7 @@ export default function ContactPage() {
                     </div>
                     <div>
                       <div style={{ fontFamily: 'DM Sans, sans-serif', fontWeight: 600, fontSize: 14, color: '#020817', marginBottom: 2 }}>Phone</div>
-                      <a href="tel:+919876543210" style={{ fontFamily: 'DM Sans, sans-serif', fontSize: 14, color: '#2132B9', textDecoration: 'none' }}>+91 9846214009</a>
+                      <a href="tel:+917736859773" style={{ fontFamily: 'DM Sans, sans-serif', fontSize: 14, color: '#2132B9', textDecoration: 'none' }}>+91 9846214009, +91  7736859773</a>
                     </div>
                   </div>
                   <div style={{ display: 'flex', gap: 14, alignItems: 'center' }}>
